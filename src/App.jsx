3af187ddc1f5f67df2600dfaa9e557ab64efb10e
Pololokka <vitorbulbovas1@hotmail.com>
2023-03-30 17:10:42 -0300

    :lipstick: feat: adicionados botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import "./App.css";
 
 import Card from "./Components/Card/Index";
+import Button from "./Components/Button/Index";
 
 function App() {
   return (
     <>
       <main className="App">
-        <Card
-          title="Super Troufer"
-          text="Doce chocolate trufado com leite condesado e limão siciliano"
-          price="2,00"
-        />
-        <Card
-          title="Amor Proibido"
-          text="Doce de leite com recheio de maracujá com sementes cristalizadas de maracujá"
-          price="3,00"
-        />
-        <Card
-          title="Rainha dos Doces"
-          text="Bolacha com paleta italiana de (morango, limão ou laranja) e mel"
-          price="4,50"
-        />
+        <h1 className="titulo titulo-hover">Confissões Da Minha Dieta</h1>
+        <div className="App-buttons__container">
+          <Button title="Fechar Pedido" />
+          <Button title="Fazer Pedido" />
+          <Button title="Limpar" />
+        </div>
+        <div className="App-cards__container">
+          <Card
+            title="Super Troufer"
+            text="Doce chocolate trufado com leite condesado e limão siciliano"
+            price="2,00"
+          />
+          <Card
+            title="Amor Proibido"
+            text="Doce de leite com recheio de maracujá com sementes cristalizadas de maracujá"
+            price="3,00"
+          />
+          <Card
+            title="Rainha dos Doces"
+            text="Bolacha com paleta italiana de (morango, limão ou laranja) e mel"
+            price="4,50"
+          />
+        </div>
       </main>
     </>
   );
---
+++ src/Components/Button/Index.jsx
@@ -0,0 +1,5 @@
+const Button = ({ title }) => {
+  return <input type="button" value={title} className="texto btn__geral" />;
+};
+
+export default Button;
